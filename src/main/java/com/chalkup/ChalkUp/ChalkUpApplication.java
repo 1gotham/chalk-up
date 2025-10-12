10432d0ec1ca4b1627f0553de365e4335ad6d8cc
Julian <juszko.zzx@gmail.com>
2025-10-12 18:44:05 +0200

    Initial commit

---
+++ src/main/java/com/chalkup/ChalkUp/ChalkUpApplication.java
@@ -0,0 +1,13 @@
+package com.chalkup.ChalkUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChalkUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChalkUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chalkup/ChalkUp/ChalkUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chalkup.ChalkUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChalkUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
