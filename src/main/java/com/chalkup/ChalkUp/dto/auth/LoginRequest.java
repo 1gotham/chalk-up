d2c6a53bec055b4adac46913e85122891bff1ba9
Julian <juszko.zzx@gmail.com>
2025-10-15 22:11:05 +0200

    Created login dtos

---
+++ src/main/java/com/chalkup/ChalkUp/dto/auth/LoginRequest.java
@@ -0,0 +1,6 @@
+package com.chalkup.ChalkUp.dto.auth;
+
+public class LoginRequest {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/chalkup/ChalkUp/dto/auth/LoginResponse.java
@@ -0,0 +1,7 @@
+package com.chalkup.ChalkUp.dto.auth;
+
+public class LoginResponse {
+    private String token;
+    private String username;
+    private String message;
+}
